e90a600bff16aae7cec76d5e43ea3094126ea43d
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-07-09 13:27:24 +0300

    update in user-controller & router

--- routes/user.router.js
+++ routes/user.router.js
@@ -4,6 +4,6 @@ import userController from "../controllers/user.controller.js";
 const router = Router();
 
 router.post("/sign-up", userController.signUp);
-router.post("/login", userController.logIn());
+// router.post("/login", userController.logIn);
 
 export default router;

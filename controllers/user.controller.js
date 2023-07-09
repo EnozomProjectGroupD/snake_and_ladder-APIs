b9176509a2f626bf25df17e73a3576ec9c5d6c85
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-07-09 13:49:10 +0300

    update the login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
 
 // #####
 // sign up
@@ -7,11 +9,11 @@ import User from "../models/User.js";
 const signUp = async (req, res) => {
   const { name, username, password } = req.body;
   try {
-    const foundUser =await User.findOne({username})
-    if(foundUser){
-      res.status(400).send('user name already exists');
-      return;
-    }
+    // const foundUser =await User.findOne({username})
+    // if(foundUser){
+    //   res.status(400).send('user name already exists');
+    //   return;
+    // }
     const user = await User.create({ name, username, password });
     res.json({ message: "User created successfully", user });
   } catch (error) {
@@ -26,15 +28,27 @@ const signUp = async (req, res) => {
 
 const logIn = async (req, res) => {
   const { username, password } = req.body;
-  console.log(username);
   try {
-    const user = await User.findOne({ username, password });
-    res.json({ message: "logIn successfully", user });
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      res.status(404).json({ message: "User not found. Please sign up" });
+      return;
+    }
+    console.log(user.password);
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    console.log(isPasswordValid);
+    if (!isPasswordValid) {
+      res.status(401).json({ message: "Invalid password" });
+      return;
+    }
+
+    res.json({ message: "Login successful", user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "user not found please sign up" });
+    res.status(500).json({ message: "Failed to log in" });
   }
 };
+
 
 
 export default {

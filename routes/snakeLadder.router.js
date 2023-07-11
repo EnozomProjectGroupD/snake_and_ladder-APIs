c45ec66a3faace6d24675d0c403684e2f12101c0
MarioWilson <89283049+MarioMedWilson@users.noreply.github.com>
2023-07-11 11:01:13 +0300

    update snakeLadder.router

--- routes/snakeLadder.router.js
+++ routes/snakeLadder.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import snakeLadderController from "../controllers/snakeLadder.controller.js";
+import {auth} from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/get-all", snakeLadderController.getAllSnakeLadders);
-router.post("/get", snakeLadderController.getSnakeLadder);
-router.post("/add", snakeLadderController.addSnakeLadder);
+router.get("/get-all", auth, snakeLadderController.getAllSnakeLadders);
+router.post("/get", auth, snakeLadderController.getSnakeLadder);
+router.post("/add", auth, snakeLadderController.addSnakeLadder);
 export default router;
